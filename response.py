78d142ec7abb579e4c3420946f1fb35dd0e4214f
MaskaMS0608 <156016026+MaskaMS0608@users.noreply.github.com>
2024-06-22 23:59:57 +0500

    Update response.py

--- response.py
+++ response.py
@@ -1,49 +1,35 @@
-from flask import Flask, jsonify, request, json
-from json import JSONEncoder
-
-class Twit:
-    def __init__(self, body, author):
-        self.body = body
-        self.author = author
-
-    def to_dict(self):
-        return {
-            'body': self.body,
-            'author': self.author
-        }
+from flask import Flask, jsonify, request
+import json
+from model.twit import Twit
 
 twits = []
 app = Flask(__name__)
 
-class CustomJSONEncoder(JSONEncoder):
+
+class CustomJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Twit):
-            return {'body': obj.body, 'author': obj.author}
+            return {"body": obj.body, "author": obj.author}
         else:
             return super().default(obj)
 
+
 app.json_encoder = CustomJSONEncoder
 
-@app.route('/get_twit', methods=['GET'])
-def get_twit():
-    twit = Twit("Пример текста", "Автор")
-    return twit.to_dict()
 
-@app.route('/twit', methods=['POST'])
+@app.route("/twit", methods=["GET"])
+def read_twits():
+    return jsonify({"twits": [twit.to_dict() for twit in twits]})
+
+
+@app.route("/twit", methods=["POST"])
 def create_twit():
+    """{"body": "Hello", "author": "MS"}"""
     twit_json = request.get_json()
-    twit = Twit(twit_json['body'], twit_json['author'])
+    twit = Twit(twit_json["body"], twit_json["author"])
     twits.append(twit)
-    return jsonify({'status': 'success'})
+    return jsonify({"status": "success"})
 
-@app.route('/twit/<int: twit_id>', methods=['DELETE'])
-def delete_twit(twit_id):
-    twit = next((t for t in twits if t['id'] == twit_id), None)
-    if twit is not None:
-        twits.remove(twit)
-        return jsonify({'status': 'success'})
-    else:
-        return jsonify({'error': 'Twit not found'}), 404
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
